1830c27e765f8c85c5aca77f2452ccd2b20bf022
Student8 <Student8@10.8.33.176>
2016-09-16 13:09:52 -0400

    object oriented programming

---
+++ Period 8 Java/src/introduction/OOPExample.java
@@ -0,0 +1,16 @@
+package introduction;
+
+public class OOPExample {
+
+	public static void main(String[] args) {
+		Student jillian = new Student("Jillian");
+		Student jason = new Student("Jason");
+		Student jordan = new Student("Jordan");
+		Student joseph = new Student("Joseph");
+		jillian.talk();
+		jason.talk();
+		jordan.talk();
+		joseph.talk();
+	}
+
+}
