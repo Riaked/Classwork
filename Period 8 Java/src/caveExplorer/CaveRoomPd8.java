0a8fb7e024bccb98363b11e727b44bd61d161366
Student8 <Student8@10.8.33.176>
2016-10-31 14:02:29 -0400

    caves

---
+++ Period 8 Java/src/caveExplorer/CaveRoomPd8.java
@@ -0,0 +1,24 @@
+package caveExplorer;
+
+public class CaveRoomPd8 {
+
+	public CaveRoomPd8(String description) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void enter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setConnection(CaveRoomPd8 caveRoomPd8) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
